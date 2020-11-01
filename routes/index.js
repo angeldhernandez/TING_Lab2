b99cd6dca032280bb21adcb227553ad9830d0d48
Ángel D. Hernández <angeld.hernandez@gmail.com>
2020-11-01 19:10:30 +0000

    sort

--- routes/index.js
+++ routes/index.js
@@ -28,4 +28,16 @@ router.get('/db', async (req, res) => {
     }
   })
 
+router.get('/db2', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('db2', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
 module.exports = router;
