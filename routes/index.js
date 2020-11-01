536909ad3e23a975e09a1544d7a9d1fc9352fe37
Ángel D. Hernández <angeld.hernandez@gmail.com>
2020-11-01 16:24:34 +0000

    update

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+		rejectUnauthorized : false
+	}
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'TING',
 												title1: 'Lab 2' });
 });
+
+router.get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect();
+      const result = await client.query('SELECT * FROM test_table');
+      const results = { 'results': (result) ? result.rows : null};
+      res.render('db', results );
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
 
 module.exports = router;
