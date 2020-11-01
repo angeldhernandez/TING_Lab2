b292239a332d8b1f2218b4c3dbea67cfb3866b3a
Ángel D. Hernández <angeld.hernandez@gmail.com>
2020-11-01 15:42:34 +0000

    db update

--- routes/db.js
+++ routes/db.js
@@ -1,9 +1,7 @@
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: 'postgres://yrjykrrwaygxcv:8e4f3d66ae34cc59cbb9f91953bdd2a8be7a7271c19df48cd72b01fc20eaca63@ec2-34-192-173-173.compute-1.amazonaws.com:5432/dca2jbt8d07jeh',
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: true
 });
 
 var express = require('express');
@@ -15,7 +13,7 @@ router.get('/db', async (req, res) => {
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM test_table');
       const results = { 'results': (result) ? result.rows : null};
-      res.render('pages/db', results );
+      res.render('/db', results );
       client.release();
     } catch (err) {
       console.error(err);
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,3 @@
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: 'postgres://yrjykrrwaygxcv:8e4f3d66ae34cc59cbb9f91953bdd2a8be7a7271c19df48cd72b01fc20eaca63@ec2-34-192-173-173.compute-1.amazonaws.com:5432/dca2jbt8d07jeh',
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-
 var express = require('express');
 var router = express.Router();
 
@@ -14,6 +6,5 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'TING',
 												title1: 'Lab 2' });
 });
-
 
 module.exports = router;
