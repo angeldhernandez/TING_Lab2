79d79873e70cd1b194e863a23e06f1ea43ab9d59
Ángel D. Hernández <angeld.hernandez@gmail.com>
2020-11-01 16:54:23 +0000

    update

--- routes/db.js
+++ routes/db.js
@@ -10,14 +10,18 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('db', { title: 'Ruta 1',
+												title1: 'Test de rutas' });
+});
+
 
 router.get('/', async (req, res) => {
     try {
       const client = await pool.connect();
       const result = await client.query('SELECT * FROM test_table');
       const results = { 'results': (result) ? result.rows : null};
-      res.render('db', results, { title: 'TING',
-																	title1: 'Lab 2' } );
+      res.render('db', results );
       client.release();
     } catch (err) {
       console.error(err);
