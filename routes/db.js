0ae9ab83de638a3d9bbb87f9f63d3cf1b174264f
Ángel D. Hernández <angeld.hernandez@gmail.com>
2020-11-01 15:31:56 +0000

    db update

--- routes/db.js
+++ routes/db.js
@@ -1,6 +1,6 @@
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: 'postgres://yrjykrrwaygxcv:8e4f3d66ae34cc59cbb9f91953bdd2a8be7a7271c19df48cd72b01fc20eaca63@ec2-34-192-173-173.compute-1.amazonaws.com:5432/dca2jbt8d07jeh',
   ssl: {
     rejectUnauthorized: false
   }
